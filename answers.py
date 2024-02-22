64ab1bc9600a9bf66a953c03b5983487b36dc993
MohanadGad38 <90938419+MohanadGad38@users.noreply.github.com>
2024-02-22 10:22:21 +0200

    First example

--- answers.py
+++ answers.py
@@ -2,8 +2,8 @@ from sklearn.linear_model import LinearRegression
 
 
 # Create X and y arrays
-X = sales_df.drop("sales", axis=1).Values
-y = sales_df["sales"].Values
+X = sales_df.drop("sales", axis=1).values
+y = sales_df["sales"].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
@@ -11,8 +11,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 reg = LinearRegression()
 
 # Fit the model to the data
-reg.fit(X, y)
+reg.fit(X_train, y_train)
 
 # Make predictions
-y_pred = reg.predict(X)
+y_pred = reg.predict(X_test)
 print("Predictions: {}, Actual Values: {}".format(y_pred[:2], y_test[:2]))
